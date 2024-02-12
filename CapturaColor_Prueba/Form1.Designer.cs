b23ecf2dc240bc4b688d8a787311d67a8a2911ce
CesatSoftware2 <software2@cesat.mx>
2024-02-12 09:20:28 -0800

    First commit

---
+++ CapturaColor_Prueba/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CapturaColor_Prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pbCamara2 = new System.Windows.Forms.PictureBox();
+            this.cboCameras = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.BtnSelectColor = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamara2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(266, 236);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // pbCamara2
+            // 
+            this.pbCamara2.Location = new System.Drawing.Point(491, 12);
+            this.pbCamara2.Name = "pbCamara2";
+            this.pbCamara2.Size = new System.Drawing.Size(266, 236);
+            this.pbCamara2.TabIndex = 1;
+            this.pbCamara2.TabStop = false;
+            // 
+            // cboCameras
+            // 
+            this.cboCameras.FormattingEnabled = true;
+            this.cboCameras.Location = new System.Drawing.Point(13, 281);
+            this.cboCameras.Name = "cboCameras";
+            this.cboCameras.Size = new System.Drawing.Size(265, 21);
+            this.cboCameras.TabIndex = 2;
+            this.cboCameras.SelectedIndexChanged += new System.EventHandler(this.cboCameras_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Location = new System.Drawing.Point(13, 317);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(265, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Camara";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(6, 19);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(253, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(5, 43);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(253, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(6, 68);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(253, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // BtnSelectColor
+            // 
+            this.BtnSelectColor.Location = new System.Drawing.Point(308, 336);
+            this.BtnSelectColor.Name = "BtnSelectColor";
+            this.BtnSelectColor.Size = new System.Drawing.Size(253, 23);
+            this.BtnSelectColor.TabIndex = 7;
+            this.BtnSelectColor.Text = "Seleccionar Color";
+            this.BtnSelectColor.UseVisualStyleBackColor = true;
+            this.BtnSelectColor.Click += new System.EventHandler(this.BtnSelectColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnSelectColor);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cboCameras);
+            this.Controls.Add(this.pbCamara2);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamara2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.PictureBox pbCamara2;
+        private System.Windows.Forms.ComboBox cboCameras;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button BtnSelectColor;
+    }
+}
+
---
+++ CapturaColor_Prueba/Form1.cs
@@ -0,0 +1,141 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge;
+
+namespace CapturaColor_Prueba
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private Color selectedColor = Color.Red; // Color predeterminado
+        private BlobCounter blobCounter = new BlobCounter();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Enumerar dispositivos de video disponibles
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in videoDevices)
+            {
+                cboCameras.Items.Add(device.Name);
+            }
+
+            if (cboCameras.Items.Count > 0)
+                cboCameras.SelectedIndex = 0;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Iniciar la captura de video con la cámara seleccionada
+            videoSource = new VideoCaptureDevice(videoDevices[cboCameras.SelectedIndex].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            videoSource.Start();
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Obtener el fotograma actual
+            Bitmap videoFrame = (Bitmap)eventArgs.Frame.Clone();
+
+            // Filtrar el color seleccionado
+            ColorFiltering colorFilter = new ColorFiltering();
+            colorFilter.Red = new IntRange(selectedColor.R - 20, selectedColor.R + 20);
+            colorFilter.Green = new IntRange(selectedColor.G - 20, selectedColor.G + 20);
+            colorFilter.Blue = new IntRange(selectedColor.B - 20, selectedColor.B + 20);
+            colorFilter.FillOutsideRange = false;
+
+            // Aplicar el filtro de color
+            Bitmap processedFrame = colorFilter.Apply(videoFrame);
+
+            // Aplicar detección de blobs
+            blobCounter.ProcessImage(processedFrame);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+
+            // Dibujar recuadro alrededor de los blobs detectados
+            foreach (Blob blob in blobs)
+            {
+                Rectangle rect = blob.Rectangle;
+                Graphics g = Graphics.FromImage(videoFrame);
+                using (Pen pen = new Pen(Color.Yellow, 2))
+                {
+                    g.DrawRectangle(pen, rect);
+                }
+            }
+            // Actualizar PictureBox con el fotograma procesado
+            if (pictureBox.InvokeRequired)
+            {
+                pictureBox.Invoke(new MethodInvoker(delegate { pictureBox.Image = videoFrame; }));
+            }
+            else
+            {
+                pictureBox.Image = videoFrame;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Detener la captura de video al cerrar el formulario
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+        }
+
+        private void cboCameras_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Cambiar la cámara en tiempo de ejecución
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                videoSource = null;
+                btnStart_Click(sender, e);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Detener la captura de video
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Cerrar la aplicación
+            this.Close();
+        }
+
+        private void BtnSelectColor_Click(object sender, EventArgs e)
+        {
+            // Mostrar el cuadro de diálogo de selección de color
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedColor = colorDialog.Color;
+                }
+            }
+        }
+    }
+}
